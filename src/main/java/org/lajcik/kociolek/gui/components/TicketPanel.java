16a3113fbb944b7757af7f850eecfe8fbc19c336
lajcik <lighteater@gmail.com>
2013-08-20 18:34:43 +0200

    Inserting buttons in order

--- src/main/java/org/lajcik/kociolek/gui/components/TicketPanel.java
+++ src/main/java/org/lajcik/kociolek/gui/components/TicketPanel.java
@@ -27,9 +27,32 @@ public class TicketPanel extends ScrollablePanel implements RentalListener {
     @Override
     public void itemRented(Rental rental) {
         TicketButton ticketButton = new TicketButton(rental);
-        add(ticketButton);
+
+        int insertIndex = -1;
+        // find a place to insert this button in an orderly fashon
+        TicketButton[] buttons = getButtons();
+        for(int i = buttons.length -1 ; i >= 0; i--) {
+            TicketButton otherButton = buttons[i];
+            if(rental.getTicketNumber() < otherButton.ticketNumber) {
+                insertIndex = i;
+                break;
+            }
+        }
+
+        add(ticketButton, insertIndex);
         buttonMap.put(rental.getTicketNumber(), ticketButton);
         revalidate();
+    }
+
+    public TicketButton[] getButtons() {
+        Component[] components = getComponents();
+        TicketButton[] result = new TicketButton[components.length];
+        int i = 0;
+        for(Component component : components) {
+            result[i++] = (TicketButton) component;
+        }
+
+        return result;
     }
 
     @Override
@@ -49,6 +72,8 @@ public class TicketPanel extends ScrollablePanel implements RentalListener {
 
     private class TicketButton extends JButton {
 
+        private int ticketNumber;
+
         private TicketButton(final Rental rental) {
             setText(Integer.toString(rental.getTicketNumber()));
             setFont(getFont().deriveFont(Font.BOLD, 20.0f));
@@ -56,12 +81,17 @@ public class TicketPanel extends ScrollablePanel implements RentalListener {
             setVerticalAlignment(CENTER);
             setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
             setMinimumSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
+            ticketNumber = rental.getTicketNumber();
             addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     new RentalDialog(null, rental);
                 }
             });
+        }
+
+        private int getTicketNumber() {
+            return ticketNumber;
         }
     }
 }
